78f49118a6dbb049029df766bba717a62ed68655
narean3929 <narean3929@gmail.com>
2025-09-03 21:19:36 +0530

    tables.java

---
+++ tables.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                for (int k = 1; k <= 10; k++) {
+                System.out.print(i*0);
+                system.out.print(j*0);
+            } System.out.println();
+
+
+
+        }
+
+
+    }
+}
+  
